4d90e253ead43ca45b58f73b7346d0a5fbc2e95a
Rahul Gupta-Iwasaki <rahul.guptaiwasaki@gmail.com>
2018-04-10 15:44:48 -0700

    Add trust endpoint.

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,73 @@
-const Koa = require('koa');
-const cors = require('@koa/cors');
+#!/usr/bin/env node
 
-const { admin } = require('./firebaseAdmin');
-const { verifyFirebaseIdToken } = require('./verifyFirebaseIdToken');
+import path from 'path';
+
+import Koa from 'koa';
+import cors from '@koa/cors';
+import Router from 'koa-router';
+
+import { getAdmin } from './firebaseAdmin';
+import  { verifyFirebaseIdToken } from './verifyFirebaseIdToken';
+
+let admin;
+if (process.env.NODE_ENV === 'test' && process.argv.length > 2) {
+    const credentialsPathArg = process.argv[2];
+    if (path.isAbsolute(credentialsPathArg)) {
+        admin = getAdmin(credentialsPathArg);
+    } else {
+        // Resolve the path relative to the cwd.
+        admin = getAdmin(path.resolve(path.join(process.cwd(), credentialsPathArg)));
+    }
+} else {
+    admin = getAdmin();
+}
+
+const db = admin.firestore();
+const members = db.collection('members');
+const operations = db.collection('operations');
 
 const app = new Koa();
 
 app.use(cors());
 
-app.use(verifyFirebaseIdToken);
+// Put endpoints that don't need the user to be authenticated above this.
+app.use(verifyFirebaseIdToken(admin));
+// Put endpoints that do need the user to be authenticated below this.
 
-app.use(async ctx => {
-    // const db = admin.database();
-    // const members = db.collections('members');
-    ctx.body = ctx.url + ' ' + ctx.state.user; // + ' ' + members;
-});
+const authenticatedRouter = new Router()
+    .param('mid', async (mid, ctx, next) => {
+        const midQuery = (await members.where('mid', '==', mid).get());
+        if (midQuery.empty) {
+            ctx.status = 404;
+            return;
+        }
+        ctx.state.toMember = midQuery.docs[0];
+        return await next();
+    })
+    .post('/api/members/:mid/trust', async ctx => {
+        const authUid = ctx.state.user.uid;
+        const authMember = await members.doc(authUid).get();
+        try {
+            const newOperation = await operations.add({
+                applied: false,
+                creator_mid: authMember.get('mid'),
+                creator_uid: authUid,
+                op_code: 'TRUST',
+                data: {
+                    to_mid: ctx.state.toMember.get('mid'),
+                    to_uid: ctx.state.toMember.id,
+                },
+            });
+            ctx.body = newOperation;
+            ctx.status = 201;
+            return;
+        } catch (error) {
+            ctx.body = error;
+            ctx.status = 500;
+        }
+    });
 
-// app.use(async ctx => {
-//     if (ctx.url === '/trust' && ctx.method === 'POST') {
-//     }
-// })
+app.use(authenticatedRouter.routes());
+app.use(authenticatedRouter.allowedMethods());
 
 app.listen(process.env.PORT || 3000);
--- src/verifyFirebaseIdToken.ts
+++ src/verifyFirebaseIdToken.ts
@@ -1,10 +1,9 @@
-const { admin } = require('./firebaseAdmin');
 
 // Express middleware that validates Firebase ID Tokens passed in the Authorization HTTP header.
 // The Firebase ID token needs to be passed as a Bearer token in the Authorization HTTP header like this:
 // `Authorization: Bearer <Firebase ID Token>`.
 // when decoded successfully, the ID Token content will be added as `req.user`.
-const verifyFirebaseIdToken = async (ctx, next) => {
+const verifyFirebaseIdToken = admin => async (ctx, next) => {
     console.log('Check if request is authorized with Firebase ID token');
 
     const { headers, cookies } = ctx;
@@ -16,29 +15,26 @@ const verifyFirebaseIdToken = async (ctx, next) => {
             'Authorization: Bearer <Firebase ID Token>',
             'or by passing a "__session" cookie.');
         ctx.status = 403;
-        ctx.body = 'Unauthorized, no Authorization header or session cookie..';
+        ctx.body = 'Unauthorized.';
         return;
     }
 
     let idToken;
     if (headers.authorization && headers.authorization.startsWith('Bearer ')) {
-        console.log('Found "Authorization" header');
         // Read the ID Token from the Authorization header.
         idToken = headers.authorization.split('Bearer ')[1];
     } else {
-        console.log('Found "__session" cookie');
         // Read the ID Token from cookie.
         idToken = cookies.__session;
     }
     try {
         const decodedIdToken = await admin.auth().verifyIdToken(idToken);
-        console.log('ID Token correctly decoded', decodedIdToken);
         ctx.state.user = decodedIdToken;
         return await next();
     } catch (error) {
         console.error('Error while verifying Firebase ID token:', error);
         ctx.status = 403;
-        ctx.body = 'Unauthorized, couldn\'t verify token.';
+        ctx.body = 'Unauthorized.';
     }
 };
 
