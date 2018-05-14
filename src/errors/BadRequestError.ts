837a118be34f5f12d0df8bd3a8ad94fec8890ca9
Rahul Gupta-Iwasaki <rahul.guptaiwasaki@gmail.com>
2018-05-14 16:55:31 -0700

    Add give endpoint.

---
+++ src/errors/BadRequestError.ts
@@ -0,0 +1,18 @@
+import ApplicationError from "./ApplicationError";
+
+/**
+ * If the network request failed (i.e. fetch threw), this error wraps it
+ */
+export default class BadRequestError extends ApplicationError {
+  constructor(message: string) {
+    super(message);
+
+    // this is necessary, typescript or not, for proper subclassing of builtins:
+    // https://github.com/Microsoft/TypeScript-wiki/blob/master/Breaking-Changes.md#extending-built-ins-like-error-array-and-map-may-no-longer-work
+    // It is also necessary for any subclasses of this class, unfortunately.
+    // TODO: once react-scripts 2.0 is out, we can use Babel Macros to do this automatically.
+    // https://github.com/facebook/create-react-app/projects/3
+    // https://github.com/loganfsmyth/babel-plugin-transform-builtin-extend
+    Object.setPrototypeOf(this, BadRequestError.prototype);
+  }
+}
