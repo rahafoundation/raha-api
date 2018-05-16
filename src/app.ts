644859cd2b64ed4d6de6595c96af206911a2fc0b
Rahul Gupta-Iwasaki <rahul.guptaiwasaki@gmail.com>
2018-05-16 16:40:53 -0700

    Move operations and members routes to new files.

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,7 @@
 #!/usr/bin/env node
 
-import crypto from "crypto";
 import path from "path";
 import { URL } from "url";
-
-import Big from "big.js";
-import asyncBusboy from "async-busboy";
-import * as coconut from "coconutjs";
-import { firestore } from "firebase-admin";
 import Storage from "@google-cloud/storage";
 import Koa from "koa";
 import bodyParser from "koa-bodyparser";
@@ -18,8 +12,9 @@ import { DocumentSnapshot, Firestore } from "@google-cloud/firestore";
 
 import { getAdmin } from "./firebaseAdmin";
 import { verifyFirebaseIdToken } from "./verifyFirebaseIdToken";
-import BadRequestError from "./errors/BadRequestError";
-import * as me from "./routes/me";
+import * as meRoutes from "./routes/me";
+import * as membersRoutes from "./routes/members";
+import * as operationsRoutes from "./routes/operations";
 
 // tslint:disable-next-line:no-var-requires
 const config = require("./config/config.json");
@@ -27,10 +22,6 @@ import {
   coconutApiKey,
   sendgridApiKey
 } from "./config/DO_NOT_COMMIT.secrets.config";
-
-const TEN_MINUTES = 1000 * 60 * 10;
-const DEFAULT_DONATION_RECIPIENT_UID = "RAHA";
-const DEFAULT_DONATION_RATE = 0.03;
 
 let admin;
 let storage: Storage.Storage;
@@ -72,180 +63,18 @@ async function validateUid(uid, ctx, next) {
   return next();
 }
 
-function getPrivateVideoRef(memberUid): Storage.File {
-  return storage
-    .bucket(config.privateVideoBucket)
-    .file(`private-video/${memberUid}/invite.mp4`);
-}
-
-function getPublicVideoRef(memberMid): Storage.File {
-  return storage
-    .bucket(config.publicVideoBucket)
-    .file(`${memberMid}/invite.mp4`);
-}
-
-async function createCoconutVideoEncodingJob(memberUid, creatorMid) {
-  const videoRef = getPrivateVideoRef(memberUid);
-
-  const temporaryAccessUrl = (await videoRef.getSignedUrl({
-    action: "read",
-    expires: Date.now() + TEN_MINUTES
-  }))[0];
-
-  const webhookUrl = new URL(
-    `members/${memberUid}/notify_video_encoded`,
-    config.apiBase
-  );
-  const uploadUrl = new URL(
-    `members/${memberUid}/upload_video`,
-    config.apiBase
-  );
-  uploadUrl.searchParams.append("mid", creatorMid);
-
-  coconut.createJob(
-    {
-      api_key: coconutApiKey,
-      source: temporaryAccessUrl,
-      webhook: webhookUrl.toString(),
-      outputs: {
-        mp4: uploadUrl.toString()
-      }
-    },
-    job => {
-      if (job.status === "ok") {
-        // tslint:disable-next-line:no-console
-        console.log("Coconut encoding job created successfully.");
-        // tslint:disable-next-line:no-console
-        console.log(job.id);
-      } else {
-        // tslint:disable-next-line:no-console
-        console.error("Coconut encoding job error", job.error_code);
-        // tslint:disable-next-line:no-console
-        console.error(job.error_message);
-      }
-    }
-  );
-}
-
-/**
- * Reads the entire stream of video data into a Buffer.
- * @param videoStream ReadableStream of video data.
- */
-function getVideoBufferFromStream(videoStream): Promise<Buffer> {
-  const buffers: Buffer[] = [];
-  const bufferPromise = new Promise<Buffer>((resolve, reject) => {
-    videoStream.on("data", (data: Buffer) => {
-      buffers.push(data);
-    });
-    videoStream.on("end", () => {
-      resolve(Buffer.concat(buffers));
-    });
-  });
-  videoStream.resume();
-  return bufferPromise;
-}
-
-/**
- * Calculate the hex-encoded sha256 hash of the video.
- * @param videoBuffer Buffer of video data.
- */
-function getHashFromVideoBuffer(videoBuffer: Buffer): string {
-  const hash = crypto.createHash("sha256");
-  hash.update(videoBuffer);
-  return hash.digest("hex");
-}
-
 const publicRouter = new Router()
   .param("uid", validateUid)
-  .get("/api/operations", async ctx => {
-    // TODO: Do we need to paginate?
-    const ops = await operations.orderBy("created_at").get();
-    const parsedOps: object[] = [];
-    ops.forEach(op =>
-      parsedOps.push({
-        id: op.id,
-        creator_mid: op.get("creator_mid"),
-        creator_uid: op.get("creator_uid"),
-        op_code: op.get("op_code"),
-        data: op.get("data")
-      })
-    );
-    ctx.body = JSON.stringify(parsedOps);
-    ctx.status = 200;
-  })
+  .get("/api/operations", operationsRoutes.index(operations))
   /* These endpoints are consumed by coconut. */
-  .post("/api/members/:uid/notify_video_encoded", async ctx => {
-    ctx.status = 200;
-  })
-  .post("/api/members/:uid/upload_video", async ctx => {
-    try {
-      const { mid } = ctx.query;
-      if (!mid) {
-        ctx.status = 400;
-        ctx.body = "Must supply MID when uploading video.";
-        return;
-      }
-
-      const publicVideoRef = getPublicVideoRef(decodeURIComponent(mid));
-      if ((await publicVideoRef.exists())[0]) {
-        ctx.status = 400;
-        ctx.body =
-          "Video already exists at intended storage destination. Cannot overwrite.";
-        return;
-      }
-
-      const reqType = ctx.request.type;
-      if (reqType !== "multipart/form-data") {
-        ctx.status = 400;
-        ctx.body = "Must submit data as multipart/form-data";
-        return;
-      }
-
-      const { files } = await asyncBusboy(ctx.req);
-      const encodedVideo = files.filter(
-        file => file.fieldname === "encoded_video"
-      );
-      if (encodedVideo.length !== 1) {
-        ctx.status = 400;
-        ctx.body = "Zero or multiple encoded videos supplied with request.";
-        return;
-      }
-
-      const videoBuf = await getVideoBufferFromStream(encodedVideo[0]);
-      const hash = getHashFromVideoBuffer(videoBuf);
-
-      const hashMappingRef = uidToVideoHash.doc(ctx.state.toMember.id);
-      if ((await hashMappingRef.get()).exists) {
-        ctx.status = 400;
-        ctx.body = "Invite video already exists for specified user.";
-        return;
-      }
-      await hashMappingRef.create({
-        hash
-      });
-
-      await publicVideoRef.save(videoBuf);
-
-      const privateVideoRef = getPrivateVideoRef(ctx.state.toMember.id);
-      if ((await privateVideoRef.exists())[0]) {
-        await privateVideoRef.delete();
-      } else {
-        // tslint:disable-next-line:no-console
-        console.warn("We expected a private video file to exist that did not.");
-      }
-
-      ctx.status = 201;
-    } catch (error) {
-      const errorMessage =
-        "An error occurred while saving an encoded video from Coconut.";
-      // tslint:disable-next-line:no-console
-      console.error(errorMessage);
-      // tslint:disable-next-line:no-console
-      console.error(error);
-      ctx.body = errorMessage;
-      ctx.status = 500;
-    }
-  });
+  .post(
+    "/api/members/:uid/notify_video_encoded",
+    membersRoutes.notifyVideoEncoded
+  )
+  .post(
+    "/api/members/:uid/upload_video",
+    membersRoutes.uploadVideo(config, storage, uidToVideoHash)
+  );
 
 app.use(publicRouter.routes());
 app.use(publicRouter.allowedMethods());
@@ -256,186 +85,20 @@ app.use(verifyFirebaseIdToken(admin));
 
 const authenticatedRouter = new Router()
   .param("uid", validateUid)
-  .post("/api/members/:uid/request_invite", async ctx => {
-    try {
-      const loggedInUid = ctx.state.user.uid;
-      const loggedInMemberRef = members.doc(loggedInUid);
-
-      if ((await loggedInMemberRef.get()).exists) {
-        ctx.status = 400;
-        ctx.body = "You have already requested an invite.";
-        return;
-      }
-
-      const { creatorMid, fullName } = ctx.request.body;
-      const requestingInviteFromMid = ctx.state.toMember.get("mid");
-      const requestingInviteFromUid = ctx.state.toMember.id;
-
-      const newOperation = {
-        creator_mid: creatorMid,
-        creator_uid: loggedInUid,
-        op_code: "REQUEST_INVITE",
-        data: {
-          full_name: fullName,
-          to_mid: requestingInviteFromMid,
-          to_uid: requestingInviteFromUid
-          // TODO: Eventually we need to extract file extension from this or a similar parameter.
-          // Currently we only handle videos uploaded as invite.mp4.
-          // video_url: ctx.request.body.videoUrl
-        },
-        created_at: firestore.FieldValue.serverTimestamp()
-      };
-      const newMember = {
-        mid: creatorMid,
-        full_name: fullName,
-        request_invite_from_uid: requestingInviteFromUid,
-        request_invite_from_mid: requestingInviteFromMid,
-        created_at: firestore.FieldValue.serverTimestamp(),
-        request_invite_block_at: null,
-        request_invite_block_seq: null,
-        request_invite_op_seq: null
-      };
-
-      createCoconutVideoEncodingJob(loggedInUid, creatorMid);
-
-      const newOperationDoc = await operations.add(newOperation);
-      await loggedInMemberRef.create(newMember);
-      ctx.body = {
-        ...(await newOperationDoc.get()).data(),
-        id: newOperationDoc.id
-      };
-      ctx.status = 201;
-    } catch (error) {
-      // tslint:disable-next-line:no-console
-      console.error(error);
-      ctx.body = "An error occurred while creating this operation.";
-      ctx.status = 500;
-    }
-  })
-  .post("/api/members/:uid/trust", async ctx => {
-    const loggedInUid = ctx.state.user.uid;
-    const loggedInMember = await members.doc(loggedInUid).get();
-    try {
-      const newOperation = {
-        creator_mid: loggedInMember.get("mid"),
-        creator_uid: loggedInUid,
-        op_code: "TRUST",
-        data: {
-          to_mid: ctx.state.toMember.get("mid"),
-          to_uid: ctx.state.toMember.id
-        },
-        created_at: firestore.FieldValue.serverTimestamp()
-      };
-      const newOperationDoc = await operations.add(newOperation);
-      ctx.body = {
-        ...(await newOperationDoc.get()).data(),
-        id: newOperationDoc.id
-      };
-      ctx.status = 201;
-    } catch (error) {
-      // tslint:disable-next-line:no-console
-      console.error(error);
-      ctx.body = "An error occurred while creating this operation.";
-      ctx.status = 500;
-    }
-  })
-  .post("/api/members/:uid/give", async ctx => {
-    try {
-      const newOperationReference = await db.runTransaction(
-        async transaction => {
-          const loggedInUid = ctx.state.user.uid;
-          const loggedInMember = await transaction.get(
-            members.doc(loggedInUid)
-          );
-          const toMember = await transaction.get(
-            (ctx.state.toMember as DocumentSnapshot).ref
-          );
-          const { amount } = ctx.request.body;
-
-          const donationRecipient = await transaction.get(
-            members.doc(
-              ctx.state.toMember.get("donation_to") ||
-                DEFAULT_DONATION_RECIPIENT_UID
-            )
-          );
-
-          if (donationRecipient === undefined) {
-            throw new BadRequestError("Donation recipient does not exist.");
-          }
-
-          const fromBalance = new Big(loggedInMember.get("raha_balance") || 0);
-          const toBalance = new Big(toMember.get("raha_balance") || 0);
-          const donationRecipientBalance = new Big(
-            donationRecipient.get("raha_balance") || 0
-          );
-
-          const donationRate = new Big(
-            ctx.state.toMember.get("donation_rate") || DEFAULT_DONATION_RATE
-          );
-          const bigAmount = new Big(amount);
-          // Round to 2 decimal places and using rounding mode 0 = round down.
-          const donationAmount = bigAmount.times(donationRate).round(2, 0);
-          const toAmount = bigAmount.minus(donationAmount);
-
-          const newFromBalance = fromBalance.minus(bigAmount);
-          if (newFromBalance.lt(0)) {
-            throw new BadRequestError("Amount exceeds account balance.");
-          }
-
-          const newOperation = {
-            creator_uid: loggedInUid,
-            op_code: "GIVE",
-            data: {
-              to_uid: toMember.id,
-              amount: toAmount.toString(),
-              donation_to: donationRecipient.id,
-              donation_amount: donationAmount.toString()
-            },
-            created_at: firestore.FieldValue.serverTimestamp()
-          };
-
-          const newOperationRef = operations.doc();
-
-          transaction
-            .update(loggedInMember.ref, {
-              raha_balance: newFromBalance.toString()
-            })
-            .update(toMember.ref, {
-              raha_balance: toBalance.plus(bigAmount).toString()
-            })
-            .update(donationRecipient.ref, {
-              raha_balance: donationRecipientBalance
-                .plus(donationAmount)
-                .toString()
-            })
-            .set(newOperationRef, newOperation);
-          return newOperationRef;
-        }
-      );
-
-      ctx.body = {
-        ...(await newOperationReference.get()).data(),
-        id: newOperationReference.id
-      };
-      ctx.status = 201;
-    } catch (error) {
-      // tslint:disable-next-line:no-console
-      console.error(error);
-      if (error instanceof BadRequestError) {
-        ctx.body = {
-          error: error.message
-        };
-        ctx.status = 400;
-      } else {
-        ctx.body = {
-          error: "An error occurred while creating this operation."
-        };
-        ctx.status = 500;
-      }
-    }
-  })
-  .post("/api/me/mint", me.mint(db, members, operations))
-  .post("/api/me/send_invite", me.sendInvite(config, sgMail, members));
+  .post(
+    "/api/members/:uid/request_invite",
+    membersRoutes.requestInvite(
+      config,
+      storage,
+      coconutApiKey,
+      members,
+      operations
+    )
+  )
+  .post("/api/members/:uid/trust", membersRoutes.trust(members, operations))
+  .post("/api/members/:uid/give", membersRoutes.give(db, members, operations))
+  .post("/api/me/mint", meRoutes.mint(db, members, operations))
+  .post("/api/me/send_invite", meRoutes.sendInvite(config, sgMail, members));
 
 app.use(authenticatedRouter.routes());
 app.use(authenticatedRouter.allowedMethods());
---
+++ src/routes/operations.ts
@@ -0,0 +1,18 @@
+import { CollectionReference } from "@google-cloud/firestore";
+
+export const index = (operations: CollectionReference) => async ctx => {
+  // TODO: Do we need to paginate?
+  const ops = await operations.orderBy("created_at").get();
+  const parsedOps: object[] = [];
+  ops.forEach(op =>
+    parsedOps.push({
+      id: op.id,
+      creator_mid: op.get("creator_mid"),
+      creator_uid: op.get("creator_uid"),
+      op_code: op.get("op_code"),
+      data: op.get("data")
+    })
+  );
+  ctx.body = JSON.stringify(parsedOps);
+  ctx.status = 200;
+};
