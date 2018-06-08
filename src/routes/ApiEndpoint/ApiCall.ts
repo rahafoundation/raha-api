5a52f588676bc5b64d6bc358e7490c9afe60ebd6
Omar Diab <me@omardiab.com>
2018-06-08 18:24:56 -0400

    add authenticated to api call spec

--- src/routes/ApiEndpoint/ApiCall.ts
+++ src/routes/ApiEndpoint/ApiCall.ts
@@ -7,11 +7,17 @@ import { ListOperationsApiCall } from "../operations";
 import { SendInviteApiCall, MintApiCall } from "../me";
 
 /**
- * Definition for the arguments you need to call a particular API endpoint
+ * Definition for the arguments you need to call a particular API endpoint. Also
+ * includes whether or not the user must be authenticated when it's called.
  */
-export interface ApiCallDefinition<Params, Body> {
+export interface ApiCallDefinition<
+  Params,
+  Body,
+  Authenticated extends boolean
+> {
   params: Params;
   body: Body;
+  authenticated: Authenticated;
 }
 
 /**
