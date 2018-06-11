408539f3254406bac8200ebef0c74639e89924c6
Omar Diab <me@omardiab.com>
2018-06-11 16:06:02 -0400

    add HttpVerb to http helpers

--- src/helpers/http.ts
+++ src/helpers/http.ts
@@ -1,5 +1,13 @@
 import * as httpStatus from "http-status";
 
+export enum HttpVerb {
+  GET = "GET",
+  POST = "POST",
+  PUT = "PUT",
+  DELETE = "DELETE",
+  PATCH = "PATCH"
+}
+
 export type HttpStatusCode = Extract<
   typeof httpStatus[keyof typeof httpStatus],
   number
