9ad0a30c111f7a8b9d93f522b13945d60527bde0
Omar Diab <me@omardiab.com>
2018-07-23 15:44:14 -0700

    Rewrite publish script

--- packages/client/scripts/publish/run.ts
+++ packages/client/scripts/publish/run.ts
@@ -8,15 +8,24 @@ import { spawnSync } from "child_process";
 const { scripts, ...restPackageJson } = packageJson;
 const { prepublishOnly, ...newScripts } = packageJson.scripts;
 
-const distDir = path.join(__dirname, "..", "..", "dist");
+// The actual published directory will not be `dist/`, but instead
+// `dist/client/src`. This is because we use `rootDirs` and `paths` in the
+// typescript config to get the types working properly, but as as result after
+// compiling the typescript the actual code for this library gets deeply nested.
+const publishDir = path.join(__dirname, "..", "..", "dist", "client", "src");
 
-console.info("Writing new package json in `dist/`...");
+// in order to achieve a flat package structure, we copy over the package json
+// to the true root of the published code. The default `prepublishOnly` script
+// prevents publishing manually, and instead mandates that you use this script;
+// so we remove it for the final, actual publishing, before copying
+// `package.json` over.
+console.info(`Writing new package.json in \`${publishDir}\`...`);
 const newPackageJson = { ...restPackageJson, ...newScripts };
 fs.writeFileSync(
-  path.join(distDir, "package.json"),
+  path.join(publishDir, "package.json"),
   JSON.stringify(newPackageJson, null, 2)
 );
 
-console.info("Written. Publishing `dist/`");
-spawnSync("npm", ["publish", distDir], { stdio: "inherit" });
+console.info(`Written. Publishing \`${publishDir}\``);
+spawnSync("npm", ["publish", publishDir], { stdio: "inherit" });
 console.info("Done.");
--- packages/shared/scripts/publish/run.ts
+++ packages/shared/scripts/publish/run.ts
@@ -8,15 +8,20 @@ import { spawnSync } from "child_process";
 const { scripts, ...restPackageJson } = packageJson;
 const { prepublishOnly, ...newScripts } = packageJson.scripts;
 
-const distDir = path.join(__dirname, "..", "..", "dist");
+const publishDir = path.join(__dirname, "..", "..", "dist");
 
-console.info("Writing new package json in `dist/`...");
+// in order to achieve a flat package structure, we copy over the package json
+// to the true root of the published code. The default `prepublishOnly` script
+// prevents publishing manually, and instead mandates that you use this script;
+// so we remove it for the final, actual publishing, before copying
+// `package.json` over.
+console.info(`Writing new package json in \`${publishDir}\`...`);
 const newPackageJson = { ...restPackageJson, ...newScripts };
 fs.writeFileSync(
-  path.join(distDir, "package.json"),
+  path.join(publishDir, "package.json"),
   JSON.stringify(newPackageJson, null, 2)
 );
 
-console.info("Written. Publishing `dist/`");
-spawnSync("npm", ["publish", distDir], { stdio: "inherit" });
+console.info(`Written. Publishing \`${publishDir}\``);
+spawnSync("npm", ["publish", publishDir], { stdio: "inherit" });
 console.info("Done.");
