98902e13f2ff43653da8d0ad59a18516057b7d2b
Rahul Gupta-Iwasaki <rahul.guptaiwasaki@gmail.com>
2018-08-13 18:20:08 +0900

    Fix comment.

--- packages/client/src/members/createMember.ts
+++ packages/client/src/members/createMember.ts
@@ -9,9 +9,10 @@ import { callApi } from "../callApi";
 /**
  * API call for a non-member to join Raha and create a new member.
  * @param fullName Full name of new member
+ * @param username New username for the new member. Must be unique
  * @param videoToken Video token of the invite, of the new member either identifying themselves or
  * being invited by the existing member. If not provided, the video will be pulled from private-video.
- * @param username New username for the new member. Must be unique
+ * @param isJointVideo boolean indicating whether both the creating and inviting members are present in the video
  * @param requestInviteFromMemberId Optional id of the member this user is requesting an invite from.
  */
 export function createMember(
