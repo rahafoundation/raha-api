1dad5b6d6fe091aa1f4acd55b66abf2692379fce
Rahul Gupta-Iwasaki <2237985+rahulgi@users.noreply.github.com>
2018-09-29 16:41:41 +0900

    Members methods was getting unwieldy. Break them out into separate files. (#115)
    * Split out give route into separate file.
    * Break out createMember endpoint.
    * Break out verify endpoint.
    * Break out trust method.

---
+++ packages/server/src/helpers/sendPushNotification.ts
@@ -0,0 +1,29 @@
+import { messaging as adminMessaging } from "firebase-admin";
+import { CollectionReference } from "@google-cloud/firestore";
+import { MemberId } from "@raha/api-shared/dist/models/identifiers";
+
+/**
+ * Send a push notification. Uses Firebase Cloud Messaging.
+ */
+export async function sendPushNotification(
+  messaging: adminMessaging.Messaging,
+  fcmTokens: CollectionReference,
+  toMemberId: MemberId,
+  title: string,
+  body: string
+) {
+  const fcmTokenData = await fcmTokens.doc(toMemberId).get();
+  const fcmToken = fcmTokenData.exists
+    ? fcmTokenData.get("fcm_token")
+    : undefined;
+
+  if (fcmToken) {
+    await messaging.send({
+      notification: {
+        title,
+        body
+      },
+      token: fcmToken
+    });
+  }
+}
---
+++ packages/server/src/routes/members/give.ts
@@ -0,0 +1,164 @@
+import { CollectionReference, Firestore } from "@google-cloud/firestore";
+import { firestore, messaging as adminMessaging } from "firebase-admin";
+
+import {
+  GiveOperation,
+  Operation,
+  OperationType
+} from "@raha/api-shared/dist/models/Operation";
+import { InsufficientBalanceError } from "@raha/api-shared/dist/errors/RahaApiError/members/give/InsufficientBalanceError";
+import { GiveApiEndpoint } from "@raha/api-shared/dist/routes/members/definitions";
+import { NotFoundError } from "@raha/api-shared/dist/errors/RahaApiError/NotFoundError";
+
+import { createApiRoute, OperationToInsert } from "..";
+import { getMemberById } from "../../collections/members";
+import { sendPushNotification } from "../../helpers/sendPushNotification";
+
+const DEFAULT_DONATION_RECIPIENT_UID = "RAHA";
+const DEFAULT_DONATION_RATE = 0.03;
+
+/**
+ * A function to notify the recipient of a Give operation.
+ */
+async function _notifyGiveRecipient(
+  messaging: adminMessaging.Messaging,
+  members: CollectionReference,
+  fcmTokens: CollectionReference,
+  giveOperation: GiveOperation
+) {
+  const { id, creator_uid, data } = giveOperation;
+  const { to_uid, amount, memo } = data;
+
+  const fromMember = await members.doc(creator_uid).get();
+  const toMember = await members.doc(to_uid).get();
+
+  if (!fromMember.exists || !toMember.exists) {
+    throw new Error(
+      `Invalid give operation with ID ${id}. One or both members does not exist.`
+    );
+  }
+  const toMemberId = toMember.id;
+
+  await sendPushNotification(
+    messaging,
+    fcmTokens,
+    toMemberId,
+    "You received Raha!",
+    `${fromMember.get("full_name")} gave you ${amount} Raha${
+      memo ? ` for ${memo}` : ""
+    }.`
+  );
+}
+
+/**
+ * Give Raha to a target member from the logged in member.
+ */
+export const give = (
+  db: Firestore,
+  messaging: adminMessaging.Messaging,
+  membersCollection: CollectionReference,
+  operations: CollectionReference,
+  fcmTokens: CollectionReference
+) =>
+  createApiRoute<GiveApiEndpoint>(async (call, loggedInMemberToken) => {
+    const newOperationReference = await db.runTransaction(async transaction => {
+      const loggedInMemberId = loggedInMemberToken.uid;
+      const loggedInMember = await transaction.get(
+        membersCollection.doc(loggedInMemberId)
+      );
+
+      const memberToGiveToId = call.params.memberId;
+      const memberToGiveTo = await getMemberById(
+        membersCollection,
+        memberToGiveToId
+      );
+      if (!memberToGiveTo) {
+        throw new NotFoundError(memberToGiveToId);
+      }
+
+      const { amount, memo } = call.body;
+
+      const donationRecipientId =
+        memberToGiveTo.get("donation_to") || DEFAULT_DONATION_RECIPIENT_UID;
+      const donationRecipient = await transaction.get(
+        membersCollection.doc(donationRecipientId)
+      );
+
+      if (donationRecipient === undefined) {
+        throw new NotFoundError(
+          donationRecipientId,
+          "Donation recipient not found."
+        );
+      }
+
+      const fromBalance = new Big(loggedInMember.get("raha_balance") || 0);
+      const toBalance = new Big(memberToGiveTo.get("raha_balance") || 0);
+      const donationRecipientBalance = new Big(
+        donationRecipient.get("raha_balance") || 0
+      );
+
+      const donationRate = new Big(
+        memberToGiveTo.get("donation_rate") || DEFAULT_DONATION_RATE
+      );
+      const bigAmount = new Big(amount);
+      // Round to 2 decimal places and using rounding mode 0 = round down.
+      const donationAmount = bigAmount.times(donationRate).round(2, 0);
+      const toAmount = bigAmount.minus(donationAmount);
+
+      const newFromBalance = fromBalance.minus(bigAmount);
+      if (newFromBalance.lt(0)) {
+        throw new InsufficientBalanceError();
+      }
+
+      const transactionMemo: string = memo ? memo : "";
+
+      const newOperation: OperationToInsert = {
+        creator_uid: loggedInMemberId,
+        op_code: OperationType.GIVE,
+        data: {
+          to_uid: memberToGiveToId,
+          amount: toAmount.toString(),
+          memo: transactionMemo,
+          donation_to: donationRecipient.id,
+          donation_amount: donationAmount.toString()
+        },
+        created_at: firestore.FieldValue.serverTimestamp()
+      };
+
+      const newOperationRef = operations.doc();
+
+      transaction
+        .update(loggedInMember.ref, {
+          raha_balance: newFromBalance.toString()
+        })
+        .update(memberToGiveTo.ref, {
+          raha_balance: toBalance.plus(bigAmount).toString()
+        })
+        .update(donationRecipient.ref, {
+          raha_balance: donationRecipientBalance.plus(donationAmount).toString()
+        })
+        .set(newOperationRef, newOperation);
+      return newOperationRef;
+    });
+
+    const newOperationData = (await newOperationReference.get()).data();
+
+    // Notify the recipient, but never let notification failure cause this API request to fail.
+    _notifyGiveRecipient(
+      messaging,
+      membersCollection,
+      fcmTokens,
+      newOperationData as GiveOperation
+    ).catch(exception => {
+      // tslint:disable-next-line:no-console
+      console.error(exception);
+    });
+
+    return {
+      body: {
+        ...newOperationData,
+        id: newOperationReference.id
+      } as Operation,
+      status: 201
+    };
+  });
