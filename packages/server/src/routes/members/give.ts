feb95f5b1d55b9407e066edd458b6c51b9bf431f
Omar Diab <me@omardiab.com>
2018-10-21 18:25:26 +0900

    don't bother with legacy give anymore, with memo present it's backwards compatible

--- packages/server/src/routes/members/give.ts
+++ packages/server/src/routes/members/give.ts
@@ -32,19 +32,15 @@ const DEFAULT_DONATION_RATE = 0.03;
 function _notificationMessageForGive(
   senderName: string,
   amount: string,
+  memo?: string,
   content?: GiveContent
 ) {
   const prefix = `${senderName} gave you ${amount} Raha`;
-  if (!content) {
-    return prefix;
-  }
-
-  switch (content.type) {
-    case GiveContentType.TEXT:
-      return `${prefix} for ${content.content}`;
-    case GiveContentType.VIDEO:
-      return `${prefix} with a video 📹`;
-  }
+  const suffixes = [
+    memo ? `for "${memo}"` : undefined,
+    content ? "and attached media! 🖼" : undefined
+  ];
+  return [prefix, ...suffixes.filter(p => !!p)].join(" ");
 }
 /**
  * A function to notify the recipient of a Give operation.
@@ -56,7 +52,7 @@ async function _notifyGiveRecipient(
   giveOperation: GiveOperation
 ) {
   const { id, creator_uid, data } = giveOperation;
-  const { to_uid, amount, content } = data;
+  const { to_uid, amount, content, memo } = data;
 
   const fromMember = await members.doc(creator_uid).get();
   const toMember = await members.doc(to_uid).get();
@@ -72,131 +68,14 @@ async function _notifyGiveRecipient(
     fcmTokens,
     toMember.id,
     "You received Raha!",
-    _notificationMessageForGive(fromMember.get("full_name"), amount, content)
+    _notificationMessageForGive(
+      fromMember.get("full_name"),
+      amount,
+      memo,
+      content
+    )
   );
 }
-
-/**
- * DELETE once we no longer support sending give requests with `memo` field,
- * instead of with `content` field
- *
- * START legacy handling code
- */
-export type LegacyGiveApiEndpoint = ApiEndpointDefinition<
-  ApiEndpointName.GIVE,
-  LegacyGiveApiCall,
-  GiveApiResponse
->;
-export const legacyGive = async (
-  db: Firestore,
-  messaging: adminMessaging.Messaging,
-  membersCollection: CollectionReference,
-  operations: CollectionReference,
-  fcmTokens: CollectionReference,
-  request: LegacyGiveApiCall["request"],
-  loggedInMemberToken: auth.DecodedIdToken
-): Promise<GiveApiResponse> => {
-  const newOperationReference = await db.runTransaction(async transaction => {
-    const loggedInMemberId = loggedInMemberToken.uid;
-    const loggedInMember = await transaction.get(
-      membersCollection.doc(loggedInMemberId)
-    );
-
-    const memberToGiveToId = request.params.memberId;
-    const memberToGiveTo = await getMemberById(
-      membersCollection,
-      memberToGiveToId
-    );
-    if (!memberToGiveTo) {
-      throw new NotFoundError(memberToGiveToId);
-    }
-
-    const donationRecipientId =
-      memberToGiveTo.get("donation_to") || DEFAULT_DONATION_RECIPIENT_UID;
-    const donationRecipient = await transaction.get(
-      membersCollection.doc(donationRecipientId)
-    );
-
-    if (donationRecipient === undefined) {
-      throw new NotFoundError(
-        donationRecipientId,
-        "Donation recipient not found."
-      );
-    }
-
-    const fromBalance = new Big(loggedInMember.get("raha_balance") || 0);
-    const toBalance = new Big(memberToGiveTo.get("raha_balance") || 0);
-    const donationRecipientBalance = new Big(
-      donationRecipient.get("raha_balance") || 0
-    );
-
-    const donationRate = new Big(
-      memberToGiveTo.get("donation_rate") || DEFAULT_DONATION_RATE
-    );
-    const bigAmount = new Big(request.body.amount);
-    // Round to 2 decimal places and using rounding mode 0 = round down.
-    const donationAmount = bigAmount.times(donationRate).round(2, 0);
-    const toAmount = bigAmount.minus(donationAmount);
-
-    const newFromBalance = fromBalance.minus(bigAmount);
-    if (newFromBalance.lt(0)) {
-      throw new InsufficientBalanceError();
-    }
-
-    const newOperation: OperationToInsert = {
-      creator_uid: loggedInMemberId,
-      op_code: OperationType.GIVE,
-      data: {
-        to_uid: memberToGiveToId,
-        amount: toAmount.toString(),
-        donation_to: donationRecipient.id,
-        donation_amount: donationAmount.toString(),
-        content: request.body.memo
-          ? { type: GiveContentType.TEXT, content: request.body.memo }
-          : undefined
-      },
-      created_at: firestore.FieldValue.serverTimestamp()
-    };
-
-    const newOperationRef = operations.doc();
-
-    transaction
-      .update(loggedInMember.ref, {
-        raha_balance: newFromBalance.toString()
-      })
-      .update(memberToGiveTo.ref, {
-        raha_balance: toBalance.plus(bigAmount).toString()
-      })
-      .update(donationRecipient.ref, {
-        raha_balance: donationRecipientBalance.plus(donationAmount).toString()
-      })
-      .set(newOperationRef, newOperation);
-    return newOperationRef;
-  });
-
-  const newOperationData = (await newOperationReference.get()).data();
-
-  // Notify the recipient, but never let notification failure cause this API request to fail.
-  _notifyGiveRecipient(
-    messaging,
-    membersCollection,
-    fcmTokens,
-    newOperationData as GiveOperation
-  ).catch(exception => {
-    // tslint:disable-next-line:no-console
-    console.error(exception);
-  });
-
-  return {
-    body: {
-      ...newOperationData,
-      id: newOperationReference.id
-    } as Operation,
-    status: 201
-  };
-};
-// END legacy handling code. also delete switch below.
-
 /**
  * Give Raha to a target member from the logged in member.
  */
@@ -207,125 +86,103 @@ export const give = (
   operations: CollectionReference,
   fcmTokens: CollectionReference
 ) =>
-  createApiRoute<LegacyGiveApiEndpoint | GiveApiEndpoint>(
-    async (request, loggedInMemberToken) => {
-      // DELETE once we no longer support sending give requests with `memo` field,
-      // instead of with `content` field
-      // START legacy handling code
-      if (!("content" in request.body)) {
-        return legacyGive(
-          db,
-          messaging,
-          membersCollection,
-          operations,
-          fcmTokens,
-          request,
-          loggedInMemberToken
-        );
-      }
-      // END legacy handling code
-
-      const newOperationReference = await db.runTransaction(
-        async transaction => {
-          const loggedInMemberId = loggedInMemberToken.uid;
-          const loggedInMember = await transaction.get(
-            membersCollection.doc(loggedInMemberId)
-          );
-
-          const memberToGiveToId = request.params.memberId;
-          const memberToGiveTo = await getMemberById(
-            membersCollection,
-            memberToGiveToId
-          );
-          if (!memberToGiveTo) {
-            throw new NotFoundError(memberToGiveToId);
-          }
-
-          const donationRecipientId =
-            memberToGiveTo.get("donation_to") || DEFAULT_DONATION_RECIPIENT_UID;
-          const donationRecipient = await transaction.get(
-            membersCollection.doc(donationRecipientId)
-          );
-
-          if (donationRecipient === undefined) {
-            throw new NotFoundError(
-              donationRecipientId,
-              "Donation recipient not found."
-            );
-          }
-
-          const fromBalance = new Big(loggedInMember.get("raha_balance") || 0);
-          const toBalance = new Big(memberToGiveTo.get("raha_balance") || 0);
-          const donationRecipientBalance = new Big(
-            donationRecipient.get("raha_balance") || 0
-          );
-
-          const donationRate = new Big(
-            memberToGiveTo.get("donation_rate") || DEFAULT_DONATION_RATE
-          );
-
-          const bigAmount = new Big(request.body.amount);
-          // Round to 2 decimal places and using rounding mode 0 = round down.
-          const donationAmount = bigAmount.times(donationRate).round(2, 0);
-          const toAmount = bigAmount.minus(donationAmount);
-
-          const newFromBalance = fromBalance.minus(bigAmount);
-          if (newFromBalance.lt(0)) {
-            throw new InsufficientBalanceError();
-          }
-
-          const newOperation: OperationToInsert = {
-            creator_uid: loggedInMemberId,
-            op_code: OperationType.GIVE,
-            data: {
-              to_uid: memberToGiveToId,
-              amount: toAmount.toString(),
-              donation_to: donationRecipient.id,
-              donation_amount: donationAmount.toString(),
-              content:
-                "content" in request.body ? request.body.content : undefined
-            },
-            created_at: firestore.FieldValue.serverTimestamp()
-          };
-
-          const newOperationRef = operations.doc();
-
-          transaction
-            .update(loggedInMember.ref, {
-              raha_balance: newFromBalance.toString()
-            })
-            .update(memberToGiveTo.ref, {
-              raha_balance: toBalance.plus(bigAmount).toString()
-            })
-            .update(donationRecipient.ref, {
-              raha_balance: donationRecipientBalance
-                .plus(donationAmount)
-                .toString()
-            })
-            .set(newOperationRef, newOperation);
-          return newOperationRef;
-        }
+  createApiRoute<GiveApiEndpoint>(async (request, loggedInMemberToken) => {
+    const newOperationReference = await db.runTransaction(async transaction => {
+      const loggedInMemberId = loggedInMemberToken.uid;
+      const loggedInMember = await transaction.get(
+        membersCollection.doc(loggedInMemberId)
       );
 
-      const newOperationData = (await newOperationReference.get()).data();
-
-      // Notify the recipient, but never let notification failure cause this API request to fail.
-      _notifyGiveRecipient(
-        messaging,
+      const memberToGiveToId = request.params.memberId;
+      const memberToGiveTo = await getMemberById(
         membersCollection,
-        fcmTokens,
-        newOperationData as GiveOperation
-      ).catch(exception => {
-        // tslint:disable-next-line:no-console
-        console.error(exception);
-      });
+        memberToGiveToId
+      );
+      if (!memberToGiveTo) {
+        throw new NotFoundError(memberToGiveToId);
+      }
 
-      return {
-        body: {
-          ...newOperationData,
-          id: newOperationReference.id
-        } as Operation,
-        status: 201
+      const donationRecipientId =
+        memberToGiveTo.get("donation_to") || DEFAULT_DONATION_RECIPIENT_UID;
+      const donationRecipient = await transaction.get(
+        membersCollection.doc(donationRecipientId)
+      );
+
+      if (donationRecipient === undefined) {
+        throw new NotFoundError(
+          donationRecipientId,
+          "Donation recipient not found."
+        );
+      }
+
+      const fromBalance = new Big(loggedInMember.get("raha_balance") || 0);
+      const toBalance = new Big(memberToGiveTo.get("raha_balance") || 0);
+      const donationRecipientBalance = new Big(
+        donationRecipient.get("raha_balance") || 0
+      );
+
+      const donationRate = new Big(
+        memberToGiveTo.get("donation_rate") || DEFAULT_DONATION_RATE
+      );
+
+      const bigAmount = new Big(request.body.amount);
+      // Round to 2 decimal places and using rounding mode 0 = round down.
+      const donationAmount = bigAmount.times(donationRate).round(2, 0);
+      const toAmount = bigAmount.minus(donationAmount);
+
+      const newFromBalance = fromBalance.minus(bigAmount);
+      if (newFromBalance.lt(0)) {
+        throw new InsufficientBalanceError();
+      }
+
+      const newOperation: OperationToInsert = {
+        creator_uid: loggedInMemberId,
+        op_code: OperationType.GIVE,
+        data: {
+          to_uid: memberToGiveToId,
+          amount: toAmount.toString(),
+          donation_to: donationRecipient.id,
+          donation_amount: donationAmount.toString(),
+          memo: "memo" in request.body ? request.body.memo : undefined,
+          content: "content" in request.body ? request.body.content : undefined
+        },
+        created_at: firestore.FieldValue.serverTimestamp()
       };
-    }
-  );
+
+      const newOperationRef = operations.doc();
+
+      transaction
+        .update(loggedInMember.ref, {
+          raha_balance: newFromBalance.toString()
+        })
+        .update(memberToGiveTo.ref, {
+          raha_balance: toBalance.plus(bigAmount).toString()
+        })
+        .update(donationRecipient.ref, {
+          raha_balance: donationRecipientBalance.plus(donationAmount).toString()
+        })
+        .set(newOperationRef, newOperation);
+      return newOperationRef;
+    });
+
+    const newOperationData = (await newOperationReference.get()).data();
+
+    // Notify the recipient, but never let notification failure cause this API request to fail.
+    _notifyGiveRecipient(
+      messaging,
+      membersCollection,
+      fcmTokens,
+      newOperationData as GiveOperation
+    ).catch(exception => {
+      // tslint:disable-next-line:no-console
+      console.error(exception);
+    });
+
+    return {
+      body: {
+        ...newOperationData,
+        id: newOperationReference.id
+      } as Operation,
+      status: 201
+    };
+  });
