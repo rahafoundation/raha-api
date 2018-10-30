0eab795905726d72a8005be0f046ae949ef8b59f
Omar Diab <me@omardiab.com>
2018-10-30 15:17:36 +0900

    media reference object types

---
+++ packages/shared/src/models/MediaReference.ts
@@ -0,0 +1,27 @@
+export interface MediaReferenceDefinition<Content> {
+  id: string;
+  content: Content & { url: string };
+}
+
+export enum MediaReferenceKind {
+  IMAGE = "IMAGE",
+  VIDEO = "VIDEO"
+}
+
+export interface MediaReferenceContent<Kind extends MediaReferenceKind> {
+  kind: MediaReferenceKind;
+  url: string;
+}
+
+export interface VideoReferenceContent
+  extends MediaReferenceContent<MediaReferenceKind.VIDEO> {
+  thumbnailUrl: string;
+}
+export type VideoReference = MediaReferenceDefinition<VideoReferenceContent>;
+
+export type ImageReferenceContent = MediaReferenceContent<
+  MediaReferenceKind.IMAGE
+>;
+export type ImageReference = MediaReferenceDefinition<ImageReferenceContent>;
+
+export type MediaReference = ImageReference | VideoReference;
