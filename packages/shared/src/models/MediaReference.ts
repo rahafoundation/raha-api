637a46db38e3c55ad2b856e3f9be9977a6e87f00
Omar Diab <me@omardiab.com>
2018-10-31 20:04:55 +0900

    add discriminating field, consistent structure to MediaReference

--- packages/shared/src/models/MediaReference.ts
+++ packages/shared/src/models/MediaReference.ts
@@ -1,18 +1,48 @@
-export interface MediaReference<AdditionalFields = {}> {
+/**
+ * Identifiers for kinds of MediaReferences
+ */
+export enum MediaReferenceKind {
+  IMAGE = "IMAGE",
+  VIDEO = "VIDEO"
+}
+
+/**
+ * Common fields available to any MediaReference content field
+ */
+export interface MediaReferenceContentBase {
+  url: string;
+}
+
+/**
+ * Shape of a reference to type of media
+ */
+export interface MediaReferenceDefinition<
+  Kind extends MediaReferenceKind,
+  Content = {}
+> {
   /**
    * unique identifier for this media. should remain the same regardless of
    * where the media is hosted (i.e. the url field below)
    */
   id: string;
-  url: string;
-  additionalFields: AdditionalFields;
+  kind: Kind;
+  content: Content & MediaReferenceContentBase;
 }
 
 /**
  * Reference to a video uploaded to Raha
  */
-export type VideoReference = MediaReference<{ thumbnailUrl: string }>;
+export type VideoReference = MediaReferenceDefinition<
+  MediaReferenceKind.VIDEO,
+  { thumbnailUrl: string }
+>;
+
 /**
  * Reference to an image uploaded to Raha
  */
-export type ImageReference = MediaReference;
+export type ImageReference = MediaReferenceDefinition<MediaReferenceKind.IMAGE>;
+
+/**
+ * Shapes of references to external media to be consumed in the Raha app
+ */
+export type MediaReference = VideoReference | ImageReference;
