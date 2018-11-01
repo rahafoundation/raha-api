c82a794b75a88cdf25ea589aecdb43ab6c9c544f
Omar Diab <me@omardiab.com>
2018-11-01 20:07:28 +0900

    add media reference validation method

--- packages/shared/src/models/MediaReference.ts
+++ packages/shared/src/models/MediaReference.ts
@@ -1,3 +1,9 @@
+import {
+  InvalidParamsError,
+  InvalidParamsDetail
+} from "../errors/RahaApiError/InvalidParamsError";
+import { Omit } from "../helpers/Omit";
+
 /**
  * Identifiers for kinds of MediaReferences
  */
@@ -36,13 +42,120 @@ export type VideoReference = MediaReferenceDefinition<
   MediaReferenceKind.VIDEO,
   { thumbnailUrl: string }
 >;
+export const VIDEO_URL_FIELDS: Array<keyof VideoReference["content"]> = [
+  "url",
+  "thumbnailUrl"
+];
 
 /**
  * Reference to an image uploaded to Raha
  */
 export type ImageReference = MediaReferenceDefinition<MediaReferenceKind.IMAGE>;
+export const IMAGE_URL_FIELDS: Array<keyof ImageReference["content"]> = ["url"];
 
 /**
  * Shapes of references to external media to be consumed in the Raha app
  */
 export type MediaReference = VideoReference | ImageReference;
+
+function _validateMediaReference<Reference extends MediaReference>({
+  fieldName,
+  mediaReference,
+  expectedUrlFields,
+  expectedKind
+}: {
+  fieldName: string;
+  mediaReference: Reference;
+  expectedUrlFields: Array<keyof Reference["content"]>;
+  expectedKind: Reference["kind"];
+}) {
+  const errors: InvalidParamsDetail[] = [];
+  const errorDetailBase: Omit<InvalidParamsDetail, "message"> = {
+    name: fieldName
+  };
+  if (mediaReference.kind !== expectedKind) {
+    errors.push({
+      ...errorDetailBase,
+      message: `Field 'kind' expected to be '${
+        MediaReferenceKind.VIDEO
+      }'; instead was ${mediaReference.kind}`
+    });
+  }
+
+  const content: Reference["content"] = mediaReference.content;
+  const invalidUrlFields: typeof expectedUrlFields = expectedUrlFields.reduce(
+    (memo, field) => {
+      const value = content[field];
+      if (!value) {
+        return [...memo, field];
+      }
+      return memo;
+    },
+    [] as typeof expectedUrlFields
+  );
+  if (invalidUrlFields.length > 0) {
+    invalidUrlFields.forEach(field => {
+      errors.push({
+        ...errorDetailBase,
+        message: `Field '${field}' expected to be a valid URL; instead was ${
+          content[field]
+        }`
+      });
+    });
+  }
+
+  if (errors.length > 0) {
+    throw new InvalidParamsError(errors);
+  }
+}
+
+export function validateMediaReference(
+  mediaReference: MediaReference,
+  fieldName: string
+) {
+  switch (mediaReference.kind) {
+    default:
+      throw new InvalidParamsError([
+        {
+          name: fieldName,
+          message: `Invalid 'kind' field: received ${
+            (mediaReference as MediaReference).kind
+          }`
+        }
+      ]);
+    case MediaReferenceKind.IMAGE:
+      return _validateMediaReference<ImageReference>({
+        fieldName,
+        mediaReference,
+        expectedUrlFields: ["url"],
+        expectedKind: MediaReferenceKind.IMAGE
+      });
+    case MediaReferenceKind.VIDEO:
+      return _validateMediaReference<VideoReference>({
+        fieldName,
+        mediaReference,
+        expectedUrlFields: VIDEO_URL_FIELDS,
+        expectedKind: MediaReferenceKind.VIDEO
+      });
+  }
+}
+
+/**
+ * Validates that a VideoReference is valid.
+ *
+ * @throws InvalidParamsDetail if not valid.
+ */
+export function validateVideoReference({
+  fieldName,
+  videoReference
+}: {
+  fieldName: string;
+  videoReference: VideoReference;
+}): void {
+  _validateMediaReference<VideoReference>({
+    fieldName,
+    mediaReference: videoReference,
+    expectedUrlFields: VIDEO_URL_FIELDS,
+    expectedKind: MediaReferenceKind.VIDEO
+  });
+}
