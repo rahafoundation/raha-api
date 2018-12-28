125180cba4070d3811c1cd6de2a896205cadec45
Tina Roh <tinaroh@gmail.com>
2018-12-28 12:38:17 -0800

    Fix some build issues and address PR comments

--- packages/shared/src/helpers/Config.ts
+++ packages/shared/src/helpers/Config.ts
@@ -9,23 +9,27 @@ export const MILLISECONDS_PER_WEEK = Big(1000 * 60 * 60 * 24 * 7);
  * history or external data sources.
  */
 export class Config {
-  public static readonly UBI_WEEKLY_RATE = Big(10)
-  public static readonly MAX_WEEKS_ACCRUE = Big(4)
+  public static readonly UBI_WEEKLY_RATE = Big(10);
+  public static readonly MAX_WEEKS_ACCRUE = Big(4);
   public static readonly REFERRAL_BONUS_PRE_SPLIT = Big(60);
   public static readonly REFERRAL_BONUS_POST_SPLIT = Big(30);
   public static readonly INVITED_BONUS_PRE_SPLIT = Big(0);
   public static readonly INVITED_BONUS_POST_SPLIT = Big(30);
   public static readonly DEFAULT_DONATION_RATE = Big(0.03);
-  public static readonly REFERRAL_SPLIT_DATE = Date.UTC(2019, 1, 1);
-  public static get REFERRAL_BONUS() {
+
+  // January 1, 2019
+  public static readonly REFERRAL_SPLIT_DATE = Date.UTC(2019, 0, 1);
+
+  public static getReferralBonus() {
     if (Date.now() < this.REFERRAL_SPLIT_DATE) {
-        return this.REFERRAL_BONUS_PRE_SPLIT;
+      return this.REFERRAL_BONUS_PRE_SPLIT;
     }
     return this.REFERRAL_BONUS_POST_SPLIT;
   }
+
   public static getInvitedBonus(memberCreatedAtMillis: number) {
     if (memberCreatedAtMillis < this.REFERRAL_SPLIT_DATE) {
-        return this.INVITED_BONUS_PRE_SPLIT;
+      return this.INVITED_BONUS_PRE_SPLIT;
     }
     return this.INVITED_BONUS_POST_SPLIT;
   }
